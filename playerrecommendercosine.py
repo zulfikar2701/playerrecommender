e7298f1bfcb166b54a4272ca6defa606eb793328
sojoya <59761474+zulfikar2701@users.noreply.github.com>
2023-06-08 21:59:31 +0700

    Update playerrecommendercosine.py

--- playerrecommendercosine.py
+++ playerrecommendercosine.py
@@ -9,10 +9,9 @@ st.set_page_config(
 )
 
 
-@st.cache_data
+@st.cache(show_spinner=False)
 def getData():
-    player_df = pd.read_pickle(
-        r'pickledata/data_out_df.pickle')
+    player_df = pd.read_pickle(r'pickledata/data_out_df.pickle')
     with open(r'pickledata/data_outfield_player_ID.pickle', 'rb') as file:
         player_ID = pickle.load(file)
     with open(r'pickledata/data_out_engine.pickle', 'rb') as file:
@@ -30,8 +29,6 @@ def getData():
 
 outfield_data, gk_data = getData()
 
-outfield_data
-
 header = st.container()
 data_info1 = st.container()
 params = st.container()
@@ -41,30 +38,31 @@ with header:
     st.title('Muhammad Zulfikar Arifin - Player Recommender')
 
 with data_info1:
-    st.markdown(
-        'Based on the 2022/23 season data for the **Big 5** European leagues :soccer:')
-
+    st.markdown('Based on the 2022/23 season data for the **Big 5** European leagues :soccer:')
     @st.cache
     def read_info(path):
         return Path(path).read_text(encoding='utf8')
+    
+    st.markdown(read_info('info.md'), unsafe_allow_html=True)
 
-col1, col2 = st.columns([1, 2.2])
-with col1:
-    radio = st.radio('Player type', ['Outfielder', 'Goalkeeper'])
-with col2:
-    if radio == 'Outfielder':
-        df, player_ID, engine = outfield_data
-    else:
-        df, player_ID, engine = gk_data
-    players = sorted(list(player_ID.keys()))
-    age_default = (15, 41)
-    query = st.selectbox('Player name', players,
-                         help='Ketik tanpa menghapus kata, untuk mencari pemain dari klub tertentu, ketikkan nama klub tersebut')
 
 with params:
     st.text(' \n')
     st.text(' \n')
     st.header('Tweak the parameters')
+
+    col1, col2 = st.columns([1, 2.2])
+    with col1:
+        radio = st.radio('Player type', ['Outfielder', 'Goalkeeper'])
+    with col2:
+        if radio == 'Outfielder':
+            df, player_ID, engine = outfield_data
+        else:
+            df, player_ID, engine = gk_data
+        players = sorted(list(player_ID.keys()))
+        age_default = (15, 41)
+        query = st.selectbox('Player name', players,
+            help='Ketik tanpa menghapus kata, untuk mencari pemain dari klub tertentu, ketikkan nama klub tersebut')
 
     col4, col5, col6, col7 = st.columns([0.7, 1, 1, 1])
     with col4:
@@ -72,71 +70,61 @@ with params:
             res, val, step = (5, 20), 10, 5
         else:
             res, val, step = (3, 10), 5, 1
-        count = st.slider(
-            'Number of results', min_value=res[0], max_value=res[1], value=val, step=step)
+        count = st.slider('Number of results', min_value=res[0], max_value=res[1], value=val, step=step)
     with col5:
         comp = st.selectbox('League', ['All', 'Premier League', 'La Liga', 'Serie A', 'Bundesliga', 'Ligue 1'],
-                            help='Leagues to get recommendations from. \'All\' leagues by default.')
+            help='Leagues to get recommendations from. \'All\' leagues by default.')
     with col6:
         comparison = st.selectbox('Comparison with', ['All positions', 'Same position'],
-                                  help='Whether to compare the selected player with all positions or just the same defined position in the dataset. \'All \
-            positions\' by default.')
+            help='Whether to compare the selected player with all positions or just the same defined position in the dataset. \'All \ positions\' by default.')
     with col7:
         age = st.slider('Age bracket', min_value=age_default[0], max_value=age_default[1], value=age_default,
-                        help='Age range to get recommendations from. Drag the sliders on either side. \'All\' ages by default.')
+        help='Age range to get recommendations from. Drag the sliders on either side. \'All\' ages by default.')
 
 with result:
     st.text(' \n')
     st.text(' \n')
     st.text(' \n')
     st.markdown('_showing recommendations for_ **{}**'.format(query))
+    
+    
+    def getRecommendations(metric, df_type, league='All', comparison='All positions', age=age_default, count=val):
+        if df_type == 'outfield':
+            df_res = df.iloc[:, [1, 3, 5, 6, 11, -1]].copy()
+        else:
+            df_res = df.iloc[:, [1, 3, 5, 6, 11]].copy()
+        df_res['Player'] = list(player_ID.keys())
+        df_res.insert(1, 'Similarity', metric)
+        df_res = df_res.sort_values(by=['Similarity'], ascending=False)
+        metric = [str(num) + '%' for num in df_res['Similarity']]
+        df_res['Similarity'] = metric
+        df_res = df_res.iloc[1:, :]
+            
+        if comparison == 'Same position' and df_type == 'outfield':
+            q_pos = list(df[df['Player'] == query.split(' (')[0]].Pos)[0]
+            df_res = df_res[df_res['Pos'] == q_pos]
+            
+        if league == 'All':
+            pass
+        else:
+            df_res = df_res[df_res['Comp'] == league]
+            
+        if age == age_default:
+            pass
+        else:
+            df_res = df_res[(df_res['Age'] >= age[0]) & (df_res['Age'] <= age[1])]
 
 
-def getRecommendations(metric, df_type, league='All', comparison='All positions', age=age_default, count=val):
-    if df_type == 'outfield':
-        df_res = df.iloc[:, [1, 3, 5, 6, 11, -1]].copy()
-    else:
-        df_res = df.iloc[:, [1, 3, 5, 6, 11]].copy()
-    df_res['Player'] = list(player_ID.keys())
-    df_res.insert(1, 'Similarity', metric)
-    df_res = df_res.sort_values(by=['Similarity'], ascending=False)
-    metric = [str(num) + '%' for num in df_res['Similarity']]
-    df_res['Similarity'] = metric
-    df_res = df_res.iloc[1:, :]
-
-    if comparison == 'Same position' and df_type == 'outfield':
-        q_pos = list(df[df['Player'] == query.split(' (')[0]].Pos)[0]
-        df_res = df_res[df_res['Pos'] == q_pos]
-
-    if league == 'All':
-        pass
-    else:
-        df_res = df_res[df_res['Comp'] == league]
-
-    if age == age_default:
-        pass
-    else:
-        df_res = df_res[(df_res['Age'] >= age[0]) & (df_res['Age'] <= age[1])]
-
-    df_res = df_res.iloc[:count, :].reset_index(drop=True)
-    df_res.index = df_res.index + 1
-    if len(df) == 2040:
-        mp90 = [str(round(num, 1)) for num in df_res['90s']]
-        df_res['90s'] = mp90
-    df_res.rename(columns={'Pos': 'Position', 'Comp': 'League'}, inplace=True)
-    return df_res
-
-
-df
-
-sims = engine[query]
-df_type = 'outfield' if len(df) == 2040 else 'gk'
-recoms = getRecommendations(
-    sims, df_type=df_type, league=comp, comparison=comparison, age=age, count=count)
-st.table(recoms)
-
-# !npm install localtunnel
-
-# !streamlit run recommender.py & >/content/logs.txt & curl ipv4.icanhazip.com
-
-# !npx localtunnel - -port 8501
+        df_res = df_res.iloc[:count, :].reset_index(drop=True)
+        df_res.index = df_res.index + 1
+        if len(df) == 2040:
+            mp90 = [str(round(num, 1)) for num in df_res['90s']]
+            df_res['90s'] = mp90
+        df_res.rename(columns={'Pos': 'Position', 'Comp': 'League'}, inplace=True)
+        return df_res
+    
+    
+    sims = engine[query]
+    df_type = 'outfield' if len(df) == 2040 else 'gk'
+    recoms = getRecommendations(sims, df_type=df_type, league=comp, comparison=comparison, age=age, count=count)
+    st.table(recoms)
